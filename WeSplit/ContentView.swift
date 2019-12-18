0b90b3cc514e7aad9ea75443fbe3dbc9d743cf46
nomizo-iox <nomizo.iox@gmail.com>
2019-12-18 01:20:03 -0600

    WeSplit with picker Picker.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,28 +9,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
     
-    let students = ["", "Samuel", "Abraham", "David"]
-    @State private var studentSelected = 0
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    // CALCULATION PER PERSON
+    var totalPerPerson: Double {
+        
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    // TOTAL AMOUNT OF THE WHOLE CHECK
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
     
     var body: some View {
         NavigationView {
             Form {
+                // AMOUNT & NUMBER OF PEOPLE SECTION
                 Section {
-                    TextField("Enter your name", text: $name)
-                    Text("Your name is \(name)")
-                }
-                Section {
-                    Picker("Select your student", selection: $studentSelected) {
-                        ForEach(0 ..< students.count) {
-                            Text(self.students[$0])
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
                         }
-                        
                     }
                 }
+                // TIP SECTION
+                Section (header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percetange", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                }
+                
+                // GRAND TOTAL SECTION
+                Section (header: Text("Grand Total")) {
+                    Text("$\(totalAmount, specifier: "%.2f")")
+                }
+                
+                // AMOUNT PER PERSONZ SECTION
+                Section (header: Text("Amount Per Person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
             }
-            .navigationBarTitle("SwiftUI")
+            .navigationBarTitle("WeSplit")
         }
     }
 }
